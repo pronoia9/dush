406423d711436ed6384e8dc5db0bb461640825dd
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-09 21:49:22 +0300

    update error messages

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,8 +1,8 @@
 import * as z from 'zod';
 
 export const signupValidationSchema = z.object({
-  name: z.string().min(2, { message: 'Name too short.' }),
-  username: z.string().min(2, { message: 'Username must be at least 2 characters.' }),
-  email: z.string().email(),
-  password: z.string().min(8, { message: 'Password too short.' })
+  name: z.string().min(2, { message: 'Hold up! Your name should be at least 2 characters. We want to remember you!' }),
+  username: z.string().min(2, { message: 'Username must be at least 2 characters. Be a trendsetter with a longer username!' }),
+  email: z.string().email({ message: 'Uh-oh! Your email looks a bit off. Check again, and let’s make sure it’s shipshape.' }),
+  password: z.string().min(8, { message: 'Whoa there! Your password should be at least 8 characters. Make it tough, like a fortress!' }),
 });
