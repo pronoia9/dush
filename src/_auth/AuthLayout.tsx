d04b30b975067a7ad42a5dc02e945ca6954e730a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-09 21:22:35 +0300

    add side image and section styles

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -8,9 +8,12 @@ export default function AuthLayout() {
       {isAuthenticated ? (
         <Navigate to='/' />
       ) : (
-        <section>
-          <Outlet />
-        </section>
+        <>
+          <section className='flex flex-1 justify-center items-center flex-col py-10'>
+            <Outlet />
+          </section>
+          <img src='/assets/images/side-img.svg' alt='sign side image' className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat' />
+        </>
       )}
     </>
   );
