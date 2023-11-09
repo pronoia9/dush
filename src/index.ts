345a6177ee5ee6425c17fffb8bca1d1cdac8148f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-09 20:28:16 +0300

    add src index for all components

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/****************  _AUTH  ****************/
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+
+/****************  _ROOT  ****************/
+
+/**************  COMPONENTS  *************/
+
+export {
+  /****************  _AUTH  ****************/
+  SigninForm,
+  SignupForm,
+
+  /****************  _ROOT  ****************/
+
+  /**************  COMPONENTS  *************/
+};
