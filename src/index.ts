64df28cfccea54d9ce9c21ba79877954f73bcf1a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-09 20:35:12 +0300

    import/export components

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 /****************  _AUTH  ****************/
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
 
 /****************  _ROOT  ****************/
-import { default as Home } from './_root/pages/Home';
+import Home from './_root/pages/Home';
+import RootLayout from './_root/RootLayout';
 
 /**************  COMPONENTS  *************/
 
@@ -11,9 +13,11 @@ export {
   /****************  _AUTH  ****************/
   SigninForm,
   SignupForm,
+  AuthLayout,
 
   /****************  _ROOT  ****************/
   Home,
+  RootLayout,
   
   /**************  COMPONENTS  *************/
 };
