ce4f5d6feb83317dacff49b654f83e248aa4d255
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-09 22:05:02 +0300

    init form, add logo + text

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,3 +1,54 @@
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import { Button, Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, Input } from '@/components';
+import { signupValidationSchema } from '@/lib/validation';
+
 export default function SignupForm() {
-  return <div>SignupForm</div>;
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof signupValidationSchema>>({
+    resolver: zodResolver(signupValidationSchema),
+    defaultValues: {
+      name: 'Dr. Penguin',
+      username: 'SpaceCowboy69',
+      email: 'intergalactic@mailer.com',
+      password: '',
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof signupValidationSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <div className='sm:w-420 flex flex-center flex-col'>
+        <img src='/assets/images/logo.svg' alt='logo' />
+        <h2 className='h3-bold md:h2-bold pt-5 sm:pt-12'>Create a new account</h2>
+        <p className='text-light-3 small-medium md:base-regular mt-2'>To use DÜSH, enter your account details</p>
+
+        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+          <FormField
+            control={form.control}
+            name='username'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder='shadcn' {...field} />
+                </FormControl>
+                <FormDescription>This is your public display name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type='submit'>Submit</Button>
+        </form>
+      </div>
+    </Form>
+  );
 }
