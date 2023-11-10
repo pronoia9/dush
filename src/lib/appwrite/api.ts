e29582e9cc4e740f7184ca27e7d4bd619822170c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-10 19:13:57 +0300

    add temp log

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -6,6 +6,8 @@ import { INewUser } from '@/types';
 export async function createUserAccount(user: INewUser) {
   try {
     const newAccount = await account.create(ID.unique(), user.email, user.password, user.name);
+    console.log('newAccount', newAccount); //! TEMP
+    
   } catch (error) {
     console.log('createUserAccount error', error);
     return error;
