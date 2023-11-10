ade4b93f0ed5f03085f76690bed42d95b131dea8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-10 18:06:48 +0300

    add index for appwrite stuff

---
+++ src/lib/appwrite/index.ts
@@ -0,0 +1,4 @@
+import { client, account, databases, storage, avatars } from '@/lib/appwrite/config';
+import { createUserAccount } from '@/lib/appwrite/api';
+
+export { client, account, databases, storage, avatars, createUserAccount };
