f97b5f5ba47ea2f13c5648d8a5bb044d2e168f8c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-10 19:24:59 +0300

    init saveUserToDB

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,15 +1,31 @@
 import { ID } from 'appwrite';
 
-import { account } from '@/lib/appwrite';
+import { account, avatars } from '@/lib/appwrite';
 import { INewUser } from '@/types';
 
 export async function createUserAccount(user: INewUser) {
   try {
     const newAccount = await account.create(ID.unique(), user.email, user.password, user.name);
-    return newAccount;
-    
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl,
+    });
+
+    return newUser;
   } catch (error) {
     console.log('createUserAccount error', error);
     return error;
   }
+}
+
+export async function saveUserToDB(user: { accountId: string; name: string; email: string; username?: string; imageUrl: URL }) {
+  try {
+  } catch (error) {}
 }
