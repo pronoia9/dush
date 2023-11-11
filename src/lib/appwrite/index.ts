ec5178951d7a058b83e83813c484f3cbce11da19
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-11 19:02:32 +0300

    add getCurrentUser

--- src/lib/appwrite/index.ts
+++ src/lib/appwrite/index.ts
@@ -1,4 +1,4 @@
 import { client, account, databases, storage, avatars } from '@/lib/appwrite/config';
-import { createUserAccount, signInAccount } from '@/lib/appwrite/api';
+import { createUserAccount, signInAccount, getCurrentUser } from '@/lib/appwrite/api';
 
-export { client, account, databases, storage, avatars, createUserAccount, signInAccount };
+export { client, account, databases, storage, avatars, createUserAccount, signInAccount, getCurrentUser };
