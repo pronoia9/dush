082a360815c062f82f242574023dd6496acc3958
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-11 23:13:20 +0300

    init QueryProvider

---
+++ src/lib/react-query/QueryProvider.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from 'react';
+
+export const QueryProvider = ({ children }: { children: ReactNode }) => {
+  return <div>QueryProvider</div>;
+};
--- src/lib/react-query/index.ts
+++ src/lib/react-query/index.ts
@@ -1,3 +1,4 @@
-import { useCreateUserAccount, useSignInAccount } from './queriesAndMutations';
+import { useCreateUserAccount, useSignInAccount } from '@/lib/react-query/queriesAndMutations';
+import { QueryProvider } from '@/lib/react-query/QueryProvider';
 
-export { useCreateUserAccount, useSignInAccount };
+export { useCreateUserAccount, useSignInAccount, QueryProvider };
