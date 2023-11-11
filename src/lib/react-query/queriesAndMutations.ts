bb32d523ea1d18c0899a7aa2b918f75f69d4fe55
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-11 18:34:39 +0300

    finish useSignInAccount

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,6 +1,6 @@
 import { useQuery, useMutation, useQueryClient, useInfiniteQuery } from '@tanstack/react-query';
 
-import { createUserAccount } from '@/lib/appwrite';
+import { createUserAccount, signInAccount } from '@/lib/appwrite';
 import { INewUser } from '@/types';
 
 export const useCreateUserAccount = () => {
@@ -9,9 +9,8 @@ export const useCreateUserAccount = () => {
   });
 };
 
-
 export const useSignInAccount = () => {
   return useMutation({
-    mutationFn: (user: INewUser) => createUserAccount(user),
+    mutationFn: (user: { email: string; password: string }) => signInAccount(user),
   });
 };
