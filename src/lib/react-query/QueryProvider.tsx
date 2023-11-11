4de4d7b6a8038f1ef274469ed40641f4a25434c4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-11 23:14:35 +0300

    finish QueryProvider

--- src/lib/react-query/QueryProvider.tsx
+++ src/lib/react-query/QueryProvider.tsx
@@ -1,5 +1,8 @@
 import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export const QueryProvider = ({ children }: { children: ReactNode }) => {
-  return <div>QueryProvider</div>;
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
