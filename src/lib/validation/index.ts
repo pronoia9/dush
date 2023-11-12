263af9ab98cb982e4815fbb7f2df96cb0911479a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-12 20:28:17 +0300

    capitalize schemas

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,13 +1,13 @@
 import * as z from 'zod';
 
-export const signupValidationSchema = z.object({
+export const SignupValidationSchema = z.object({
   name: z.string().min(2, { message: 'Hold up! Your name should be at least 2 characters. We want to remember you!' }),
   username: z.string().min(2, { message: 'Username must be at least 2 characters. Be a trendsetter with a longer username!' }),
   email: z.string().email({ message: 'Uh-oh! Your email looks a bit off. Check again, and let’s make sure it’s shipshape.' }),
   password: z.string().min(8, { message: 'Whoa there! Your password should be at least 8 characters. Make it tough, like a fortress!' }),
 });
 
-export const signinValidationSchema = z.object({
+export const SigninValidationSchema = z.object({
   email: z.string().email({ message: 'Uh-oh! Your email looks a bit off. Check again, and let’s make sure it’s shipshape.' }),
   password: z.string().min(8, { message: 'Whoa there! Your password should be at least 8 characters. Is is tough, like a fortress!' }),
 });
