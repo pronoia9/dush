3386e698e439361586c1a9804262c0d541bd0c8c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-12 20:46:38 +0300

    add navigation components

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,3 +1,18 @@
+import { Outlet } from 'react-router-dom';
+
+import { Bottombar, LeftSidebar, Topbar } from '@/components';
+
 export default function RootLayout() {
-  return <div>RootLayout</div>;
+  return (
+    <div className='w-full md:flex'>
+      <Topbar />
+      <LeftSidebar />
+
+      <section className='flex flex-1 h-full'>
+        <Outlet />
+      </section>
+      
+      <Bottombar />
+    </div>
+  );
 }
