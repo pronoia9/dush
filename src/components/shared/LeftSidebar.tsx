b84215f97dd7ed9a535cbedf78088eb2e25d7d4e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-13 20:42:01 +0300

    Add logout button to LeftSidebar

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -46,6 +46,11 @@ export default function LeftSidebar() {
           })}
         </ul>
       </div>
+
+      <Button variant='ghost' className='shad-button_ghost' onClick={() => signOut()}>
+        <img src='/assets/icons/logout.svg' alt='logout' />
+        <p className='small-medium lg:base-medium'>Logout</p>
+      </Button>
     </nav>
   );
 }
