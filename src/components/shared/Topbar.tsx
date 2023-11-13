d5756f0c9cc0ee75345874540809b3d7056ad4ab
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-13 19:59:49 +0300

    add useeffect + navigate if signed out

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,10 +1,16 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { Button } from '@/components';
 import { useSignOutAccount } from '@/lib/react-query';
 
 const Topbar = () => {
+  const navigate = useNavigate();
   const { mutate: signOut, isSuccess } = useSignOutAccount();
+
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess]);
 
   return (
     <section className='topbar'>
