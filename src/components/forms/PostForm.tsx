4006dd87fa83d283393635ffab562d501f0712b7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-14 23:30:29 +0300

    remove sign in errors, add post validation

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -3,24 +3,22 @@ import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import { Button, FileUploader, Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, Input, Textarea } from '@/components';
+import { PostValidation } from '@/lib/validation';
 
-const formSchema = z.object({
-  caption: z.string().min(2, {
-    message: 'Username must be at least 2 characters.',
-  }),
-});
-
-export default function PostForm() {
+export default function PostForm({ post }: any) {
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof PostValidation>>({
+    resolver: zodResolver(PostValidation),
     defaultValues: {
-      caption: '',
+      caption: post ? post?.caption : '',
+      file: [],
+      location: post ? post?.location : '',
+      tags: post ? post.tags.join(', ') : '',
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof PostValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
@@ -50,7 +48,7 @@ export default function PostForm() {
             <FormItem>
               <FormLabel className='shad-form_label'>Add Photos</FormLabel>
               <FormControl>
-                <FileUploader />
+                <FileUploader fieldChange={field.onChange} mediaUrl={post?.imageUrl} />
               </FormControl>
               <FormMessage className='shad-form_message' />
             </FormItem>
