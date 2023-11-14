0fe3ac8498be63882c8e807e013f14747101afe4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-15 01:06:55 +0300

    init QUERY_KEYS

--- src/lib/react-query/index.ts
+++ src/lib/react-query/index.ts
@@ -1,7 +1,9 @@
 import { useCreateUserAccount, useSignInAccount, useSignOutAccount, useCreatePost } from '@/lib/react-query/queriesAndMutations';
+import { QUERY_KEYS } from '@/lib/react-query/queryKeys';
 import { QueryProvider } from '@/lib/react-query/QueryProvider';
 
 export {
   useCreateUserAccount, useSignInAccount, useSignOutAccount, useCreatePost,
-  QueryProvider
+  QUERY_KEYS,
+  QueryProvider,
 };
