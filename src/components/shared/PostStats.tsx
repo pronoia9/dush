13599535d61a3566e34ca00ff30ba1ede89c6fef
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-15 22:16:09 +0300

    add init post stats

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,4 +1,16 @@
-const PostStats = () => {
-  return <div>PostStats</div>;
-};
-export default PostStats;
+import { Models } from 'appwrite';
+
+export default function PostStats({ post, userId }: { post: Models.Document; userId: string }) {
+  return (
+    <div className='flex justify-between items-center z-20'>
+      <div className='flex gap-2 mr-5'>
+        <img src={`/assets/icons/like.svg`} alt='like' width={20} height={20} onClick={() => {}} className='cursor-pointer' />
+        <p className='small-medium lg:base-medium'>0</p>
+      </div>
+
+      <div className='flex gap-2'>
+        <img src={`/assets/icons/save.svg`} alt='save' width={20} height={20} onClick={() => {}} className='cursor-pointer' />
+      </div>
+    </div>
+  );
+}
