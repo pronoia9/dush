c11719c7a72e573f8d3f6e34efb2f3cee235d22b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-15 22:56:41 +0300

    Add savePost and deleteSavedPost functions to API

--- src/lib/appwrite/index.ts
+++ src/lib/appwrite/index.ts
@@ -1,7 +1,7 @@
 import { appwriteConfig, client, account, databases, storage, avatars } from '@/lib/appwrite/config';
-import { createUserAccount, signInAccount, getCurrentUser, signOutAccount, createPost, getRecentPosts, likePost, } from '@/lib/appwrite/api';
+import { createUserAccount, signInAccount, getCurrentUser, signOutAccount, createPost, getRecentPosts, likePost, savePost, deleteSavedPost, } from '@/lib/appwrite/api';
 
 export {
   appwriteConfig, client, account, databases, storage, avatars,
-  createUserAccount, signInAccount, getCurrentUser, signOutAccount, createPost, getRecentPosts, likePost,
+  createUserAccount, signInAccount, getCurrentUser, signOutAccount, createPost, getRecentPosts, likePost, savePost, deleteSavedPost,
 };
