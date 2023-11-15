737e77004957b8b17e535331c4ebf4de7c5c3444
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-15 18:36:27 +0300

    add post card

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,3 +1,27 @@
+import { Loader, PostCard } from '@/components';
+import { useGetRecentPosts } from '@/lib/react-query';
+import { Models } from 'appwrite';
+
 export default function Home() {
-  return <div>Home</div>;
+  const { data: posts, isLoading: isPostsLoading, isError: isErrorPosts } = useGetRecentPosts();
+
+  return (
+    <div className='flex flex-1'>
+      <div className='home-container'>
+        <div className='home-posts'>
+          <h2 className='h3-bold md:h2-bold text-left w-full'>Home Feed</h2>
+          {isPostsLoading && !posts ? (
+            <Loader />
+          ) : (
+            <ul className='flex flex-col flex-1 gap-9 w-full'>
+              {posts?.documents.map((post: Models.Document) => {
+                console.log(post);
+                return <li className=''><PostCard post={post} /></li>;
+              })}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
