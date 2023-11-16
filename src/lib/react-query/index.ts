4fbe70217b9b233228a109b54a3fd41ab0feb224
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-16 18:43:03 +0300

    Add useUpdatePost to queriesAndMutations

--- src/lib/react-query/index.ts
+++ src/lib/react-query/index.ts
@@ -1,9 +1,9 @@
-import { useCreateUserAccount, useSignInAccount, useSignOutAccount, useCreatePost, useGetRecentPosts, useLikePost, useSavePost, useDeleteSavedPost, useGetCurrentUser, useGetPostById, } from '@/lib/react-query/queriesAndMutations';
+import { useCreateUserAccount, useSignInAccount, useSignOutAccount, useCreatePost, useGetRecentPosts, useLikePost, useSavePost, useDeleteSavedPost, useGetCurrentUser, useGetPostById, useUpdatePost, } from '@/lib/react-query/queriesAndMutations';
 import { QUERY_KEYS } from '@/lib/react-query/queryKeys';
 import { QueryProvider } from '@/lib/react-query/QueryProvider';
 
 export {
-  useCreateUserAccount, useSignInAccount, useSignOutAccount, useCreatePost, useGetRecentPosts, useLikePost, useSavePost, useDeleteSavedPost, useGetCurrentUser, useGetPostById,
+  useCreateUserAccount, useSignInAccount, useSignOutAccount, useCreatePost, useGetRecentPosts, useLikePost, useSavePost, useDeleteSavedPost, useGetCurrentUser, useGetPostById, useUpdatePost,
   QUERY_KEYS,
   QueryProvider,
 };
