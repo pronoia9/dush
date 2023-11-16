f5f4ade7f5da4be6cdf5d1a51e084a3fe98bea74
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-16 18:44:54 +0300

    Add useDeletePost to queriesAndMutations

--- src/lib/react-query/index.ts
+++ src/lib/react-query/index.ts
@@ -1,9 +1,9 @@
-import { useCreateUserAccount, useSignInAccount, useSignOutAccount, useCreatePost, useGetRecentPosts, useLikePost, useSavePost, useDeleteSavedPost, useGetCurrentUser, useGetPostById, useUpdatePost, } from '@/lib/react-query/queriesAndMutations';
+import { useCreateUserAccount, useSignInAccount, useSignOutAccount, useCreatePost, useGetRecentPosts, useLikePost, useSavePost, useDeleteSavedPost, useGetCurrentUser, useGetPostById, useUpdatePost, useDeletePost, } from '@/lib/react-query/queriesAndMutations';
 import { QUERY_KEYS } from '@/lib/react-query/queryKeys';
 import { QueryProvider } from '@/lib/react-query/QueryProvider';
 
 export {
-  useCreateUserAccount, useSignInAccount, useSignOutAccount, useCreatePost, useGetRecentPosts, useLikePost, useSavePost, useDeleteSavedPost, useGetCurrentUser, useGetPostById, useUpdatePost,
+  useCreateUserAccount, useSignInAccount, useSignOutAccount, useCreatePost, useGetRecentPosts, useLikePost, useSavePost, useDeleteSavedPost, useGetCurrentUser, useGetPostById, useUpdatePost, useDeletePost,
   QUERY_KEYS,
   QueryProvider,
 };
