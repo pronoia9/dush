067a9e45bf9216b78f507ed722c09a02ab65820f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-16 17:16:33 +0300

    Add check for liked posts in PostStats component

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -3,6 +3,7 @@ import { Models } from 'appwrite';
 import { useUserContext } from '@/context';
 import { useDeleteSavedPost, useLikePost, useSavePost } from '@/lib/react-query';
 import { useEffect, useState } from 'react';
+import { checkIsLiked } from '@/lib/utils';
 
 export default function PostStats({ post, userId }: { post: Models.Document; userId: string }) {
   const likesList = post.likes.map((user: Models.Document) => user.$id);
@@ -24,7 +25,7 @@ export default function PostStats({ post, userId }: { post: Models.Document; use
   return (
     <div className='flex justify-between items-center z-20'>
       <div className='flex gap-2 mr-5'>
-        <img src={`/assets/icons/like.svg`} alt='like' width={20} height={20} onClick={() => {}} className='cursor-pointer' />
+        <img src={`/assets/icons/like${checkIsLiked(likes, userId) ? 'd' : ''}.svg`} alt='like' width={20} height={20} onClick={() => {}} className='cursor-pointer' />
         <p className='small-medium lg:base-medium'>0</p>
       </div>
 
