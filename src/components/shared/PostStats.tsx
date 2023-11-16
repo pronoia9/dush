e244f29f54f4c7d85b3d386c76a008abcace805a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-16 17:15:27 +0300

    init states + functions

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,6 +1,26 @@
 import { Models } from 'appwrite';
 
+import { useUserContext } from '@/context';
+import { useDeleteSavedPost, useLikePost, useSavePost } from '@/lib/react-query';
+import { useEffect, useState } from 'react';
+
 export default function PostStats({ post, userId }: { post: Models.Document; userId: string }) {
+  const likesList = post.likes.map((user: Models.Document) => user.$id);
+  const [likes, setLikes] = useState(likesList);
+  const [isSaved, setIsSaved] = useState(false);
+
+  const { mutate: likePost } = useLikePost(),
+    { mutate: savePost } = useSavePost(),
+    { mutate: deleteSavedPost } = useDeleteSavedPost();
+
+  const { user } = useUserContext();
+
+  const handleLikePost = () => { };
+  
+  const handleSavePost = () => {};
+
+  useEffect(() => {}, [likes]);
+
   return (
     <div className='flex justify-between items-center z-20'>
       <div className='flex gap-2 mr-5'>
