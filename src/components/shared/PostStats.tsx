184017d81ed0281d1a67601a2c4085c1b43a9954
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-16 17:17:26 +0300

    Fix like button functionality in PostStats component

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -16,8 +16,8 @@ export default function PostStats({ post, userId }: { post: Models.Document; use
 
   const { user } = useUserContext();
 
-  const handleLikePost = () => { };
-  
+  const handleLikePost = () => {};
+
   const handleSavePost = () => {};
 
   useEffect(() => {}, [likes]);
@@ -25,8 +25,15 @@ export default function PostStats({ post, userId }: { post: Models.Document; use
   return (
     <div className='flex justify-between items-center z-20'>
       <div className='flex gap-2 mr-5'>
-        <img src={`/assets/icons/like${checkIsLiked(likes, userId) ? 'd' : ''}.svg`} alt='like' width={20} height={20} onClick={handleLikePost} className='cursor-pointer' />
-        <p className='small-medium lg:base-medium'>0</p>
+        <img
+          src={`/assets/icons/like${checkIsLiked(likes, userId) ? 'd' : ''}.svg`}
+          alt='like'
+          width={20}
+          height={20}
+          onClick={handleLikePost}
+          className='cursor-pointer'
+        />
+        <p className='small-medium lg:base-medium'>{likes.length}</p>
       </div>
 
       <div className='flex gap-2'>
