294f6650b3dad0646e5d8bdc0d50b6ed894a38bb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-16 18:10:26 +0300

    Add getPostById query to react-query

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,6 +1,17 @@
 import { useQuery, useMutation, useQueryClient, useInfiniteQuery } from '@tanstack/react-query';
 
-import { createPost, createUserAccount, deleteSavedPost, getCurrentUser, getRecentPosts, likePost, savePost, signInAccount, signOutAccount } from '@/lib/appwrite';
+import {
+  createPost,
+  createUserAccount,
+  deleteSavedPost,
+  getCurrentUser,
+  getPostById,
+  getRecentPosts,
+  likePost,
+  savePost,
+  signInAccount,
+  signOutAccount,
+} from '@/lib/appwrite';
 import { QUERY_KEYS } from '@/lib/react-query';
 import { INewPost, INewUser } from '@/types';
 
@@ -84,5 +95,13 @@ export const useGetCurrentUser = () => {
   return useQuery({
     queryKey: [QUERY_KEYS.GET_CURRENT_USER],
     queryFn: getCurrentUser,
+  });
+};
+
+export const useGetPostById = (postId: string) => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_POST_BY_ID, postId],
+    queryFn: () => getPostById(postId),
+    enabled: !!postId,
   });
 };
