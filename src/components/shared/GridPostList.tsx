6609172b1fd4cf9b6d3fb1e3f07b2ab074607669
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-17 23:25:08 +0300

    init image

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -1,3 +1,26 @@
-export default function GridPostList() {
-  return <div>GridPostList</div>;
+import { Link } from 'react-router-dom';
+import { Models } from 'appwrite';
+
+import { useUserContext } from '@/context';
+
+type GridPostListProps = {
+  posts: Models.Document[];
+  showUser?: boolean;
+  showStats?: boolean;
+};
+
+export default function GridPostList({ posts, showUser = true, showStats = true }: GridPostListProps) {
+  const { user } = useUserContext();
+
+  return (
+    <ul className='grid-container'>
+      {posts.map((post, index) => (
+        <li key={post.$id} className='relative min-w-80 h-80'>
+          <Link to={`/posts/${post.$id}`} className='grid-post_link'>
+            <img src={post.imageUrl} alt='post image' className='h-full w-full object-cover' />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
