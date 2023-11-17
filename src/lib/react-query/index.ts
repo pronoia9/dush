959cba545d9da5c9f2885aae9994a243b90d2eef
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-17 22:09:32 +0300

    update filename + import paths

--- src/lib/react-query/index.ts
+++ src/lib/react-query/index.ts
@@ -1,9 +1,12 @@
-import { useCreateUserAccount, useSignInAccount, useSignOutAccount, useCreatePost, useGetRecentPosts, useLikePost, useSavePost, useDeleteSavedPost, useGetCurrentUser, useGetPostById, useUpdatePost, useDeletePost, } from '@/lib/react-query/queriesAndMutations';
+import { useCreateUserAccount, useSignInAccount, useSignOutAccount, useCreatePost, useGetRecentPosts, useLikePost, useSavePost, useDeleteSavedPost, useGetCurrentUser, useGetPostById, useUpdatePost, useDeletePost, } from '@/lib/react-query/queries';
 import { QUERY_KEYS } from '@/lib/react-query/queryKeys';
 import { QueryProvider } from '@/lib/react-query/QueryProvider';
 
 export {
+  // QUERIES
   useCreateUserAccount, useSignInAccount, useSignOutAccount, useCreatePost, useGetRecentPosts, useLikePost, useSavePost, useDeleteSavedPost, useGetCurrentUser, useGetPostById, useUpdatePost, useDeletePost,
+  // QUERY KEYS
   QUERY_KEYS,
+  // PROVIDER
   QueryProvider,
 };
