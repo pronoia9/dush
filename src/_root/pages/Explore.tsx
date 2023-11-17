108c9cb0455bd7df11abf3585b9f395232c12f0c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-17 23:18:02 +0300

    Refactor Explore page to include search functionality and display popular posts

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,9 +1,19 @@
 import { useState } from 'react';
 
-import { Input } from '@/components';
+import { GridPostList, Input, Loader, SearchResults } from '@/components';
+import useDebounce from '@/hooks/useDebounce';
+import { useGetPosts, useSearchPosts } from '@/lib/react-query';
 
 export default function Explore() {
   const [searchValue, setSearchValue] = useState('');
+  const debouncedValue = useDebounce(searchValue, 500);
+  const { data: posts, fetchNextPage, hasNextPage } = useGetPosts();
+  const { data: searchedPost, isLoading: isSearching } = useSearchPosts(debouncedValue);
+
+  if (!posts) return <div className='flex-center w-full h-full'><Loader /></div>;
+
+  const shouldShowSearchResults = searchValue !== ''; //! TEMP
+  const shouldShowPosts = !shouldShowSearchResults && posts?.pages?.every((item: any) => item?.documents.length === 0); //! TEMP
 
   return (
     <div className='explore-container'>
@@ -14,6 +24,24 @@ export default function Explore() {
           <img src='/public/assets/icons/search.svg' alt='search' width={24} height={24} />
           <Input type='text' placeholder='Search' className='explore-search' value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
         </div>
+      </div>
+
+      <div className='flex-between w-full max-w-5xl mt-16 mb-7'>
+        <h3 className='body-bold md:h3-bold'>Popular Today</h3>
+        <div className='flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer'>
+          <p className='small-medium md:base-medium text-light-2'>All</p>
+          <img src='/assets/icons/filter.svg' alt='filter' width={20} height={20} />
+        </div>
+      </div>
+
+      <div className='flex flex-wrap gap-9 w-full max-w-5xl'>
+        {shouldShowSearchResults ? (
+          <SearchResults />
+        ) : shouldShowPosts ? (
+          <p className='text-light-4 mt-10 text-center w-full'>End of posts</p>
+        ) : (
+          posts?.pages.map((item, index) => <GridPostList key={`explore-page-${index}`} posts={item.documents} />)
+        )}
       </div>
     </div>
   );
