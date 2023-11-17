2609248bb1652f1e2c9b320f394cc89255e27fec
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-17 22:27:10 +0300

    Refactor import statements in react-query index.ts

--- src/lib/react-query/index.ts
+++ src/lib/react-query/index.ts
@@ -1,10 +1,46 @@
-import { useCreateUserAccount, useSignInAccount, useSignOutAccount, useCreatePost, useGetRecentPosts, useLikePost, useSavePost, useDeleteSavedPost, useGetCurrentUser, useGetPostById, useUpdatePost, useDeletePost, } from '@/lib/react-query/queries';
+// index.ts
+import {
+  // Auth Queries
+  useCreateUserAccount,
+  useSignInAccount,
+  useSignOutAccount,
+  // Post Queries
+  useCreatePost,
+  useGetRecentPosts,
+  useLikePost,
+  useSavePost,
+  useDeleteSavedPost,
+  useGetCurrentUser,
+  useGetPostById,
+  useUpdatePost,
+  useDeletePost,
+  // User Queries
+  useGetUsers,
+  useGetUserById,
+  useUpdateUser,
+} from '@/lib/react-query/queries';
 import { QUERY_KEYS } from '@/lib/react-query/queryKeys';
 import { QueryProvider } from '@/lib/react-query/QueryProvider';
 
 export {
-  // QUERIES
-  useCreateUserAccount, useSignInAccount, useSignOutAccount, useCreatePost, useGetRecentPosts, useLikePost, useSavePost, useDeleteSavedPost, useGetCurrentUser, useGetPostById, useUpdatePost, useDeletePost,
+  // Auth Queries
+  useCreateUserAccount,
+  useSignInAccount,
+  useSignOutAccount,
+  // Post Queries
+  useCreatePost,
+  useGetRecentPosts,
+  useLikePost,
+  useSavePost,
+  useDeleteSavedPost,
+  useGetCurrentUser,
+  useGetPostById,
+  useUpdatePost,
+  useDeletePost,
+  // User Queries
+  useGetUsers,
+  useGetUserById,
+  useUpdateUser,
   // QUERY KEYS
   QUERY_KEYS,
   // PROVIDER
