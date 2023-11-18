31dc8dd98d639507b37f0906f2d5a6a154c30bbb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-18 18:11:01 +0300

    Add user information and post statistics to GridPostList component

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { Models } from 'appwrite';
 
 import { useUserContext } from '@/context';
+import { PostStats } from '..';
 
 type GridPostListProps = {
   posts: Models.Document[];
@@ -19,6 +20,17 @@ export default function GridPostList({ posts, showUser = true, showStats = true 
           <Link to={`/posts/${post.$id}`} className='grid-post_link'>
             <img src={post.imageUrl} alt='post image' className='h-full w-full object-cover' />
           </Link>
+
+          <div className='grid-post_user'>
+            {showUser && (
+              <div className='flex items-center justify-start gap-2 flex-1'>
+                <img src={post.creator.imageUrl} alt='creator' className='h-8 w-8 rounded-full' />
+                <p className='line-clamp-1'>{post.creator.name}</p>
+              </div>
+            )}
+
+            {showStats && <PostStats post={post} userId={user.id} />}
+          </div>
         </li>
       ))}
     </ul>
