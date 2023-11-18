8451f87a2dc9a05453e130f8020c3c772b3cb5fa
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-18 18:13:49 +0300

    Refactor SearchResults component to accept props

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,3 +1,10 @@
-export default function SearchResults() {
+import { Models } from "appwrite";
+
+type SearchResultsProps = {
+  searchedPosts: Models.Document[];
+  isSearchFetching: boolean;
+};
+
+export default function SearchResults({ searchedPosts, isSearchFetching }: SearchResultsProps) {
   return <div>SearchResults</div>;
 }
