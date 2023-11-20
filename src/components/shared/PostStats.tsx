ecd25c3d3d4fa7c55c750bc78245b5ac56da5697
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-20 22:01:52 +0300

    Refactor PostStats component to improve code readability and performance

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,21 +1,22 @@
 import { MouseEvent, useEffect, useState } from 'react';
 import { Models } from 'appwrite';
+import { useLocation } from 'react-router-dom';
 
-import { Loader } from '@/components';
 import { useDeleteSavedPost, useGetCurrentUser, useLikePost, useSavePost } from '@/lib/react-query';
 import { checkIsLiked } from '@/lib/utils';
 
 export default function PostStats({ post, userId }: { post: Models.Document; userId: string }) {
+  const location = useLocation();
   // Extract the list of user IDs who liked the post from the post object
   const likesList = post.likes.map((user: Models.Document) => user.$id);
   // Initialize state variables for likes and saved status
-  const [likes, setLikes] = useState(likesList);
+  const [likes, setLikes] = useState<string[]>(likesList);
   const [isSaved, setIsSaved] = useState(false);
 
   // Destructure and get mutations and data from custom React Query hooks
   const { mutate: likePost } = useLikePost(),
-    { mutate: savePost, isLoading: isSavingPost } = useSavePost(),
-    { mutate: deleteSavedPost, isLoading: isDeletingPost } = useDeleteSavedPost(),
+    { mutate: savePost } = useSavePost(),
+    { mutate: deleteSavedPost } = useDeleteSavedPost(),
     { data: currentUser } = useGetCurrentUser();
 
   // Check if the post is already saved by the current user
@@ -27,10 +28,10 @@ export default function PostStats({ post, userId }: { post: Models.Document; use
     e.stopPropagation();
 
     // Toggle the like status for the current user
-    let newLikes = checkIsLiked(likes, userId) ? likes.filter((id: string) => id !== userId) : [...likes, userId];
+    let likesArray = checkIsLiked(likes, userId) ? likes.filter((id: string) => id !== userId) : [...likes, userId];
     // Update state with the new likes array and trigger the likePost mutation
-    setLikes(newLikes);
-    likePost({ postId: post.$id, likesArray: newLikes });
+    setLikes(likesArray);
+    likePost({ postId: post.$id, likesArray });
   };
 
   // Handle the save action for a post
@@ -40,16 +41,18 @@ export default function PostStats({ post, userId }: { post: Models.Document; use
     // Toggle the saved status and trigger the appropriate mutation
     if (savedPostRecord) {
       setIsSaved(false);
-      deleteSavedPost(savedPostRecord.$id);
-    } else {
-      setIsSaved(true);
-      savePost({ postId: post.$id, userId });
+      return deleteSavedPost(savedPostRecord.$id);
     }
+
+    savePost({ postId: post.$id, userId });
+    setIsSaved(true);
   };
+
+  const containerStyles = location.pathname.startsWith('/profile') ? ' w-full' : '';
 
   // Render the component UI
   return (
-    <div className='flex justify-between items-center z-20'>
+    <div className={`flex justify-between items-center z-20${containerStyles}`}>
       <div className='flex gap-2 mr-5'>
         {/* Like button with dynamic icon based on whether the user has liked the post */}
         <img
@@ -66,18 +69,14 @@ export default function PostStats({ post, userId }: { post: Models.Document; use
 
       <div className='flex gap-2'>
         {/* Save button with dynamic icon based on whether the post is saved */}
-        {isSavingPost || isDeletingPost ? (
-          <Loader />
-        ) : (
-          <img
-            src={`/assets/icons/save${isSaved ? 'd' : ''}.svg`}
-            alt='save'
-            width={20}
-            height={20}
-            onClick={handleSavePost}
-            className='cursor-pointer'
-          />
-        )}
+        <img
+          src={`/assets/icons/save${isSaved ? 'd' : ''}.svg`}
+          alt='save'
+          width={20}
+          height={20}
+          onClick={handleSavePost}
+          className='cursor-pointer'
+        />
       </div>
     </div>
   );
