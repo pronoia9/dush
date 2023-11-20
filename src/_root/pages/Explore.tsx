1349891aa9c0c14bce305ba88462ec0dcfbdc53e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-20 21:27:40 +0300

    format

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -5,17 +5,6 @@ import { GridPostList, Input, Loader } from '@/components';
 import useDebounce from '@/hooks/useDebounce';
 import { useGetPosts, useSearchPosts } from '@/lib/react-query';
 
-export type SearchResultProps = {
-  isSearchFetching: boolean;
-  searchedPosts: any;
-};
-
-const SearchResults = ({ isSearchFetching, searchedPosts }: SearchResultProps) => {
-  if (isSearchFetching) return <Loader />;
-  else if (searchedPosts && searchedPosts.documents.length > 0) return <GridPostList posts={searchedPosts.documents} />;
-  else return <p className='text-light-4 mt-10 text-center w-full'>No results found.</p>;
-};
-
 export default function Explore() {
   const { ref, inView } = useInView({ threshold: 0 });
   const [searchValue, setSearchValue] = useState('');
@@ -83,3 +72,9 @@ export default function Explore() {
     </div>
   );
 }
+
+const SearchResults = ({ isSearchFetching, searchedPosts }: { isSearchFetching: boolean; searchedPosts: any }) => {
+  if (isSearchFetching) return <Loader />;
+  else if (searchedPosts && searchedPosts.documents.length > 0) return <GridPostList posts={searchedPosts.documents} />;
+  else return <p className='text-light-4 mt-10 text-center w-full'>No results found.</p>;
+};
