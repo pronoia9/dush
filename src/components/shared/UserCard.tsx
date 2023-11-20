51b50f03afc17e29ec690b220b08cbbf56c8b4f8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-20 21:58:59 +0300

    Refactor UserCard component to include user profile link and follow button

--- src/components/shared/UserCard.tsx
+++ src/components/shared/UserCard.tsx
@@ -1,3 +1,21 @@
-export default function UserCard() {
-  return <div>UserCard</div>;
+import { Link } from 'react-router-dom';
+import { Models } from 'appwrite';
+
+import { Button } from '@/components';
+
+export default function UserCard({ user }: { user: Models.Document }) {
+  return (
+    <Link to={`/profile/${user.$id}`} className='user-card'>
+      <img src={user.imageUrl || '/assets/icons/profile-placeholder.svg'} alt='creator' className='rounded-full w-14 h-14' />
+
+      <div className='flex-center flex-col gap-1'>
+        <p className='base-medium text-light-1 text-center line-clamp-1'>{user.name}</p>
+        <p className='small-regular text-light-3 text-center line-clamp-1'>@{user.username}</p>
+      </div>
+
+      <Button type='button' size='sm' className='shad-button_primary px-5'>
+        Follow
+      </Button>
+    </Link>
+  );
 }
