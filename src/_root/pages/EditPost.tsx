6914fc525be627dd65c1b9539562acf3b5090f29
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-20 21:33:32 +0300

    Refactor EditPost component to handle loading state

--- src/_root/pages/EditPost.tsx
+++ src/_root/pages/EditPost.tsx
@@ -3,20 +3,25 @@ import { useParams } from 'react-router-dom';
 import { Loader, PostForm } from '@/components';
 import { useGetPostById } from '@/lib/react-query';
 
-export default function EditPost({ }) {
+export default function EditPost({}) {
   const { id } = useParams();
   const { data: post, isLoading } = useGetPostById(id || '');
 
-  if (isLoading) return <Loader />;
+  if (isLoading)
+    return (
+      <div className='flex-center w-full h-full'>
+        <Loader />
+      </div>
+    );
   return (
     <div className='flex flex-1'>
       <div className='common-container'>
-        <div className='max-w-5xl flex-start gap-3 justify-start w-full'>
-          <img src='/assets/icons/add-post.svg' width={36} height={36} alt='add post' />
+        <div className='flex-start gap-3 justify-start w-full max-w-5xl'>
+          <img src='/assets/icons/edit.svg' width={36} height={36} alt='edit' className='invert-white' />
           <h2 className='h3-bold md:h2-bold text-left w-full'>Edit Post</h2>
         </div>
 
-        <PostForm post={post} action='Update' />
+        {isLoading ? <Loader /> : <PostForm action='Update' post={post} />}
       </div>
     </div>
   );
