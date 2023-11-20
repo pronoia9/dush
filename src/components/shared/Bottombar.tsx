f931282665502607a85cc49ff9f021626910e9d1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-20 21:58:36 +0300

    Update Bottombar component imports and image class

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,21 +1,21 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation } from 'react-router-dom';
 
-import { bottombarLinks } from "@/constants";
+import { bottombarLinks } from '@/constants';
 
 const Bottombar = () => {
   const { pathname } = useLocation();
   
   return (
     <section className='bottom-bar'>
-      {bottombarLinks.map((link, index) => {
+      {bottombarLinks.map((link) => {
         const isActive = pathname === link.route;
         return (
           <Link
-            key={`bottombar-link-${index}`}
+            key={`bottombar-${link.label}`}
             to={link.route}
             className={`flex-center flex-col gap-1 p-2 transition${isActive ? ' bg-primary-500 rounded-[10px]' : ''}`}
           >
-            <img src={link.imgURL} alt={link.label} className={`group-hover:invert-white${isActive ? ' invert-white' : ''}`} width={16} height={16} />
+            <img src={link.imgURL} alt={link.label} className={`${isActive ? ' invert-white' : ''}`} width={16} height={16} />
             <p className='tiny-medium text-light-2'>{link.label}</p>
           </Link>
         );
