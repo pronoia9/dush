af11d0a38aa41b912c480d5a65c3fc5f74b24aff
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-28 18:25:26 +0300

    Update updateUser

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -394,9 +394,48 @@ export async function getUserById(userId: string) {
   }
 }
 
-// TODO ============================== UPDATE USER
+// ============================== UPDATE USER
 export async function updateUser(user: IUpdateUser) {
+  const hasFileToUpdate = user.file.length > 0;
+
   try {
+    let image = { imageUrl: user.imageUrl, imageId: user.imageId };
+
+    if (hasFileToUpdate) {
+      // Upload new file to appwrite storage
+      const uploadedFile = await uploadFile(user.file[0]);
+      if (!uploadedFile) throw Error;
+
+      // Get new file url
+      const fileUrl = getFilePreview(uploadedFile.$id);
+      if (!fileUrl) {
+        await deleteFile(uploadedFile.$id);
+        throw Error;
+      }
+
+      image = { ...image, imageUrl: fileUrl, imageId: uploadedFile.$id };
+    }
+
+    //  Update user
+    const updatedUser = await databases.updateDocument(appwriteConfig.databaseId, appwriteConfig.usersCollectionId, user.userId, {
+      name: user.name,
+      bio: user.bio,
+      imageUrl: image.imageUrl,
+      imageId: image.imageId,
+    });
+
+    // Failed to update
+    if (!updatedUser) {
+      // Delete new file that has been recently uploaded
+      if (hasFileToUpdate) await deleteFile(image.imageId);
+      // If no new file uploaded, just throw error
+      throw Error;
+    }
+
+    // Safely delete old file after successful update
+    if (user.imageId && hasFileToUpdate) await deleteFile(user.imageId);
+
+    return updatedUser;
   } catch (error) {
     console.error(
       'Updating user... Error! The user is undergoing a metamorphosis into a digital butterfly. Please wait for their magnificent transformation to complete.',
