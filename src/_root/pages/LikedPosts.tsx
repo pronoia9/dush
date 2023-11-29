737b565b4b720db7c7d03bf43586db358c77de4b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-29 17:03:42 +0300

    finish LikedPosts component to display loader and handle empty liked posts

--- src/_root/pages/LikedPosts.tsx
+++ src/_root/pages/LikedPosts.tsx
@@ -1,4 +1,18 @@
-const LikedPosts = () => {
-  return <div>LikedPosts</div>;
-};
-export default LikedPosts;
+import { useGetCurrentUser } from '@/lib/react-query/queries';
+
+import { GridPostList, Loader } from '@/components';
+
+export default function LikedPosts() {
+  const { data: currentUser } = useGetCurrentUser();
+
+  return !currentUser ? (
+    <div className='flex-center w-full h-full'>
+      <Loader />
+    </div>
+  ) : (
+    <>
+      {currentUser.liked.length === 0 && <p className='text-light-4'>No liked posts</p>}
+      <GridPostList posts={currentUser.liked} showStats={false} />
+    </>
+  );
+}
