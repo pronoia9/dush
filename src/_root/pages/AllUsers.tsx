79db11eed434ca7e623fded98e1149c0be9b36ef
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-12-02 17:52:18 +0300

    finish init all users component

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,4 +1,31 @@
-const AllUsers = () => {
-  return <div>AllUsers</div>;
-};
-export default AllUsers;
+import { Loader, UserCard, useToast } from '@/components';
+import { useGetUsers } from '@/lib/react-query';
+
+export default function AllUsers() {
+  const { toast } = useToast();
+  const { data: creators, isLoading, isError: isErrorCreators } = useGetUsers();
+
+  if (isErrorCreators) {
+    toast({ title: '' });
+    return;
+  }
+
+  return (
+    <div className='common-container'>
+      <div className='user-container'>
+        <h2 className='h3-bold md:h2-bold text-left w-full'>All Users</h2>
+        {isLoading && !creators ? (
+          <Loader />
+        ) : (
+          <ul className='user-grid'>
+            {creators?.documents.map((creator) => (
+              <li key={creator?.$id} className='flex-1 min-w-[200px] w-full'>
+                <UserCard user={creator} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+}
