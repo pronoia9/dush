8a1d9d1dd359ae98474ff4e20cf8824bcb74dd36
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-12-03 21:56:37 +0300

    add routes

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -15,6 +15,19 @@ export default function Profile() {
       <Loader />
     </div>
   ) : (
-    <div className='profile-container'></div>
+    <div className='profile-container'>
+      <Routes>
+        <Route index element={<GridPostList posts={currentUser.posts} showUser={false} />} />
+        {currentUser.$id === user.id && <Route path='/liked-posts' element={<LikedPosts />} />}
+      </Routes>
+      <Outlet />
+    </div>
   );
 }
+
+const StatBlock = ({ value, label }: { value: string | number; label: string }) => (
+  <div className='flex-center gap-2'>
+    <p className='small-semibold lg:body-bold text-primary-500'>{value}</p>
+    <p className='small-medium lg:base-medium text-light-2'>{label}</p>
+  </div>
+);
