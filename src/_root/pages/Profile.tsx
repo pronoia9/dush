afbf8ea996aa8bf007682a6f24d741a79222348c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-12-03 21:56:00 +0300

    add loader

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -1,4 +1,20 @@
-const Profile = () => {
-  return <div>Profile</div>;
-};
-export default Profile;
+import { Route, Routes, Link, Outlet, useParams, useLocation } from 'react-router-dom';
+
+import { Button, GridPostList, LikedPosts, Loader } from '@/components';
+import { useUserContext } from '@/context';
+import { useGetUserById } from '@/lib/react-query';
+
+export default function Profile() {
+  const { pathname } = useLocation();
+  const { id } = useParams();
+  const { user } = useUserContext();
+  const { data: currentUser } = useGetUserById(id || '');
+
+  return !currentUser ? (
+    <div className='flex-center w-full h-full'>
+      <Loader />
+    </div>
+  ) : (
+    <div className='profile-container'></div>
+  );
+}
