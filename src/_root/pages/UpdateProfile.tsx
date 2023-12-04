d7a943c745aea2976278330c99ed15b60c292625
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-12-04 19:24:30 +0300

    add imports

--- src/_root/pages/UpdateProfile.tsx
+++ src/_root/pages/UpdateProfile.tsx
@@ -1,4 +1,27 @@
-const UpdateProfile = () => {
+import { useNavigate, useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Textarea,
+  Input,
+  Button,
+  ProfileUploader,
+  Loader,
+  useToast,
+} from '@/components';
+
+import { useUserContext } from '@/context';
+import { useGetUserById, useUpdateUser } from '@/lib/react-query';
+import { ProfileValidation } from '@/lib/validation';
+
+export default function UpdateProfile() {
   return <div>UpdateProfile</div>;
-};
-export default UpdateProfile;
+}
