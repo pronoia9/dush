c337cc9198b15724e67cc8456f851ca825905fcf
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-24 15:43:12 -0400

    add defaultTheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   darkMode: ['class'],
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
